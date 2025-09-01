4004af6992c379612248249cf8bbc4b76f5ac4f4
Anoop George <anoopgeorge418@gmail.com>
2025-09-01 22:09:08 +0530

    Projects Initial Setup Completed!

---
+++ brainflow-web/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500">This is a heading</h1>
+      <h2>This is working as i wanted this to be!</h2>
+    </>
+  )
+}
+
+export default App
